7335843d36469b0a2c465a7488094956a05a0c8f
Raman Prasad <raman_prasad@harvard.edu>
2013-11-14 13:41:36 -0500

    update to wordpress plugin for "show admin form link"

--- mcb-theme/functions.php
+++ mcb-theme/functions.php
@@ -1,25 +1,29 @@
 <?php
 
-/* ------------------------------------------------
-    Login page customizations
------------------------------------------------- */
-/* login page: remove the lost password link */
-function remove_lostpassword_text ( $text ) {
+// ------------------------------------------------ 
+// login page: remove the lost password link */
+// ------------------------------------------------ 
+/*function xremove_lostpassword_text ( $text ) {
          if ($text == 'Lost your password?'){$text = '<a id="lnk_show_login_form">(Show Admin Login Form)</a>';}
                 return $text;
          }
-add_filter( 'gettext', 'remove_lostpassword_text' );
-
-/* end: remove_lostpassword_text */
-
-/*
-    Hide the WP login form but give a link "Show Admin Login Form" to reveal it
 */
+//add_filter( 'gettext', 'remove_lostpassword_text' );
+
+// end: remove_lostpassword_text ------------------
+
+// ------------------------------------------------ 
+//    Hide the WP login form but give a link "Show Admin Login Form" to reveal it
+// ------------------------------------------------ 
 function my_login_stylesheet() { ?>
     <link rel="stylesheet" id="custom_wp_admin_css"  href="<?php echo get_bloginfo( 'stylesheet_directory' ) . '/login-style.css'; ?>" type="text/css" media="all" />
     <script type='text/javascript' src='/wp-includes/js/jquery/jquery.js'></script>
      <script>
     jQuery(document).ready(function($){
+
+        // add "Show Admin Login Form" link
+        $( "#login" ).append('<a id="lnk_show_login_form">(Show Admin Login Form)</a>');
+        
         $("#lnk_show_login_form").click(function() {
             $('#loginform').show();   
             $("#lnk_show_login_form").hide()
