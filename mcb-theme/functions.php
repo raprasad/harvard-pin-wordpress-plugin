69deeba4ca9fbe2780d61df63cfb91f25b4e866a
Raman Prasad <raman_prasad@harvard.edu>
2013-11-21 14:09:18 -0500

    working with a title filter to break "postdoc" line in main nav

--- mcb-theme/functions.php
+++ mcb-theme/functions.php
@@ -12,6 +12,40 @@
 
 // end: remove_lostpassword_text ------------------
 
+function mcb_menu_title_markup( $title, $id ){
+//    if ( is_nav_menu_item ( $id ) ){
+        if ($id==677){
+            //$title = str_ireplace( "#BR#", "<br/>", $title );
+            $title = str_ireplace( "Research", "<br/>Research", $title );
+        //    return 'pdocs';
+        }
+  //  }
+    ///return "$title - $id";
+    return $title;
+}
+//add_filter( 'the_title', 'mcb_menu_title_markup', 10, 2 );
+
+// ------------------------------------------------ 
+//    PDF filter on Media Page
+// ------------------------------------------------
+function modify_post_mime_types( $post_mime_types ) {  
+  
+    // select the mime type, here: 'application/pdf'  
+    // then we define an array with the label values  
+  
+    $post_mime_types['application/pdf'] = array( __( 'PDFs' ), __( 'Manage PDFs' ), _n_noop( 'PDF <span class="count">(%s)</span>', 'PDFs <span class="count">(%s)</span>' ) );  
+
+//    $post_mime_types['application/pdf'] = array( __( 'PDFs' ), __( 'Manage PDFs' ), _n_noop( 'PDF <span class="count">(%s)</span>', 'PDFs <span class="count">(%s)</span>' ) );  
+  
+    // then we return the $post_mime_types variable  
+    return $post_mime_types;  
+  
+}  
+  
+// Add Filter Hook  
+add_filter( 'post_mime_types', 'modify_post_mime_types' );
+
+
 // ------------------------------------------------ 
 //    Hide the WP login form but give a link "Show Admin Login Form" to reveal it
 // ------------------------------------------------ 
