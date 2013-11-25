7d10a656e620c4a9250270276f80052fb4287c4a
Raman Prasad <raman_prasad@harvard.edu>
2013-11-25 16:41:50 -0500

    Several peripheral changes
    - HU pin: .htaccess redirects instead of actual directory with PHP
    redirect
    - “out of network page”
       - RewriteRules to keep admin part of site private

--- hu-authz/callback/index.php
+++ hu-authz/callback/index.php
@@ -1,6 +1,5 @@
 <?php
     header("Status: 301 Moved Permanently");
-    //header("Location:https://mcbintranet.unix.fas.harvard.edu/wp-login.php?". $_SERVER['QUERY_STRING']);
     header("Location:https://internal.mcb.harvard.edu/wp-login.php?". $_SERVER['QUERY_STRING']);
     
     exit;
---
+++ out-of-network/index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <!--link rel="shortcut icon" href="../../docs-assets/ico/favicon.png"-->
+
+    <title>MCB Intranet</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="/out-of-network/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="/out-of-network/css/jumbotron-narrow.css" rel="stylesheet">
+
+    <!-- Just for debugging purposes. Don't actually copy this line! -->
+    <!--[if lt IE 9]><script src="/out-of-network/js/ie8-responsive-file-warning.js"></script><![endif]-->
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+
+    <div class="container">
+      <div class="header">
+        <ul class="nav nav-pills pull-right">
+        <!--
+          <li class="active"><a href="#">Home</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Contact</a></li>
+        -->
+        </ul>
+        <h3 class="text-muted">MCB Intranet</h3>
+      </div>
+
+      <div class="jumbotron">
+          <button type="button" class="btn btn-xs btn-warning">Note</button>
+        <p class="lead"><br />To access the MCB Intranet, your computer/device must be using:</p>
+        
+            <ul>
+                <li>The Harvard Network OR</li> 
+                <li>The <a href="http://huit.harvard.edu/vpn-services">Harvard Virtual Private Network (VPN)</a></li>
+            </ul>
+        
+      </div>
+      
+      <div class="row marketing">
+        <div class="col-lg-6">
+          <p>View the <a href="https://www.mcb.harvard.edu">Department of Molecular and Cellular Biology Website</a><p>
+          <p>View the <a href="http://www.harvard.edu">Harvard University Website</a><p>
+        </div>
+      </div>
+
+      <div class="footer">
+        <p> Copyright &copy; <?php echo date("Y"); ?> The President and Fellows of Harvard College</p>
+      </div>
+
+    </div> <!-- /container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+  </body>
+</html>
